3975156dd8304aed01b8c754af69b3ae7fcd960e
Abhinay Jangde <abhinayjangde@gmail.com>
2025-08-05 12:37:20 +0530

    implemented redis for caching

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -1,5 +1,6 @@
-import type { Request,Response } from "express";
+import type { Request, Response } from "express";
 import Url from "../models/url.model.js";
+import redis from "../lib/client.js";
 
 export const createURL = async (req: Request, res: Response) => {
 
@@ -27,38 +28,45 @@ export const createURL = async (req: Request, res: Response) => {
         originalUrl,
         shortUrl: urlCode,
     });
-    
-    if(!savedURL){
+
+    if (!savedURL) {
         return res.status(400).json({
             success: false,
             error: "Error while saving user in database"
         })
     }
-    
+
     return res.render("home", {
         url: shortUrl
     });
-    // return res.status(201).json({
-    //     originalUrl,
-    //     shortUrl,   
-    //     message: 'Short URL created successfully'
-    // });
 };
 
 export const redirectURL = async (req: Request, res: Response) => {
     const { urlCode } = req.params;
+
     if (!urlCode) {
         return res.status(400).json({ error: 'URL code is required' });
     }
 
-   const existingUrl = await Url.findOne({ shortUrl: urlCode });
-    if (!existingUrl) {
-        return res.status(404).json({ error: 'Short URL not found' });
+    try {
+        // Check Redis cache first
+        const cachedUrl = await redis.get(urlCode);
+        if (cachedUrl) {
+            console.log("Cache hit ", urlCode);
+            return res.status(302).redirect(cachedUrl);
+        }
+        console.log("Cache miss", urlCode);
+        const existingUrl = await Url.findOne({ shortUrl: urlCode });
+        if (!existingUrl) {
+            return res.status(404).json({ error: 'Short URL not found' });
+        }
+        const originalUrl = existingUrl.originalUrl;
+        // Store in Redis cache for future requests
+        await redis.set(urlCode, originalUrl, 'EX', 60 * 60); // 1 Hour
+        return res.status(302).redirect(originalUrl);
+    } catch (error) {
+        console.error("Error occurred while redirecting URL:", error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-    const originalUrl = existingUrl.originalUrl;
-    if (!originalUrl) {
-        return res.status(404).json({ error: 'Original URL not found' });
-    }
-    res.status(302).redirect(originalUrl);
 };
 
